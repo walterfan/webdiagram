935046723b200fff61c3184d70516199e33d6480
Walter Fan <walterfan@qq.com>
2022-06-05 12:09:53 +0800

    update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,12 @@
 #source ./venv/bin/activate
 export FLASK_DEBUG=1
 export FLASK_APP=app.py
-flask run --host=0.0.0.0 --port=8000
+
+if [ $# -eq 0 ]  # Must have command-line args to demo script.
+then
+  echo "Please invoke this script with one or more command-line arguments."
+  flask run --host=0.0.0.0 --port=8000
+else
+  flask run --host=0.0.0.0 --port=$1
+fi
+
